4e3c13becd2f08e8d7b1668fb5f8feb00003d966
LAPTOP-0TH2B7ID\martin <martintrap1@gmail.com>
2021-03-23 11:29:50 +0100

    spawnamanager
    started work on spawnmanaager. late push

---
+++ Game/PPG/Assets/Scripts/Spawnmanager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnmanager : MonoBehaviour
+{
+    public GameObject[] carPrefabs;
+    public float spawnHeight = 0.3f;
+    public float spawnSite1 = Vector3(244.5f, spawnHeight, 203.5f);
+    public float spawnSite2 = Vector3(236, spawnHeight, 200);
+    public float spawnSite3 = Vector3(242.7f, spawnHeight, 194);
+    public float spawnSite4 = Vector3(234.5f, spawnHeight, 191);
+    public float spawnSite5 = Vector3(241, spawnHeight, 185f);
+    public float spawnSite6 = Vector3(232.75f, spawnHeight, 181.7f);
+    public float spawnSite7 = Vector3(239.5f, spawnHeight, 175.4f);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
