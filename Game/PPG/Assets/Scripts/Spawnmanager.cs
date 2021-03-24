6cfdc796724a32de92400947c673117b85b1a0ab
LAPTOP-0TH2B7ID\martin <martintrap1@gmail.com>
2021-03-24 13:20:18 +0100

    gamemanager progress
    added list to gamemanager.

--- Game/PPG/Assets/Scripts/Spawnmanager.cs
+++ Game/PPG/Assets/Scripts/Spawnmanager.cs
@@ -6,18 +6,31 @@ public class Spawnmanager : MonoBehaviour
 {
     public GameObject[] carPrefabs;
     public float spawnHeight = 0.3f;
-    public float spawnSite1 = Vector3(244.5f, spawnHeight, 203.5f);
-    public float spawnSite2 = Vector3(236, spawnHeight, 200);
-    public float spawnSite3 = Vector3(242.7f, spawnHeight, 194);
-    public float spawnSite4 = Vector3(234.5f, spawnHeight, 191);
-    public float spawnSite5 = Vector3(241, spawnHeight, 185f);
-    public float spawnSite6 = Vector3(232.75f, spawnHeight, 181.7f);
-    public float spawnSite7 = Vector3(239.5f, spawnHeight, 175.4f);
+    public Vector3 spawnSite1 = new Vector3(244.5f, spawnHeight, 203.5f);
+    public Vector3 spawnSite2 = new Vector3(236, spawnHeight, 200);
+    public Vector3 spawnSite3 = new Vector3(242.7f, spawnHeight, 194);
+    public Vector3 spawnSite4 = new Vector3(234.5f, spawnHeight, 191);
+    public Vector3 spawnSite5 = new Vector3(241, spawnHeight, 185f);
+    public Vector3 spawnSite6 = new Vector3(232.75f, spawnHeight, 181.7f);
+    public Vector3 spawnSite7 = new Vector3(239.5f, spawnHeight, 175.4f);
+    
+    public List<Vector3> spawnGrid = new List<Vector3>();
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnGrid.Add(spawnSite1);
+        spawnGrid.Add(spawnSite2);
+        spawnGrid.Add(spawnSite3);
+        spawnGrid.Add(spawnSite4);
+        spawnGrid.Add(spawnSite5);
+        spawnGrid.Add(spawnSite6);
+
+        int currentPos = spawnGrid.Count - 1;
+        Random.Range(spawnGrid[0], spawnGrid[currentPos]);
+        spawnGrid.Remove(spawnGrid[currentPos]);
     }
+
+
 
     // Update is called once per frame
     void Update()
